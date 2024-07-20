2cf1e1ef6e883eec521c9733b940effaa760e5a1
Robert Duraj <rduraj@gmail.com>
2024-07-20 20:13:19 +0200

    fix: add 404 products not found error

--- src/interface/http/errors/NotFoundError.ts
+++ src/interface/http/errors/NotFoundError.ts
@@ -2,7 +2,7 @@ import { HttpError } from 'interface/http/errors/HttpError.ts';
 import { HttpStatusCode } from 'interface/http/HttpStatusCode.ts';
 
 export class NotFoundError extends HttpError {
-  constructor() {
-    super(`Not found.`, HttpStatusCode.NOT_FOUND);
+  constructor(message = 'Not found.') {
+    super(message, HttpStatusCode.NOT_FOUND);
   }
 }
--- src/interface/http/products/findProductsRoute.ts
+++ src/interface/http/products/findProductsRoute.ts
@@ -1,9 +1,14 @@
 import { Request, Response } from 'express';
 import { ProductFacade } from 'products/application/ProductFacade.ts';
 import { HttpStatusCode } from 'interface/http/HttpStatusCode.ts';
+import { NotFoundError } from 'interface/http/errors/NotFoundError.ts';
 export const findProductsRoute =
   (products: ProductFacade) => async (req: Request, res: Response) => {
     const listOfProducts = await products.getListOfProducts();
+
+    if (listOfProducts) {
+      throw new NotFoundError('Products not found');
+    }
 
     return res.status(HttpStatusCode.SUCCESS).send(listOfProducts);
   };
